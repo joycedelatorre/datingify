624d7224594aa7174fd3411abe54b4264e98001d
joyce <joycedelatorre87@gmail.com>
2017-12-16 10:54:53 -0600

    PASS:display view.html when connected to port 3000

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, "view.html"));
+})
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+})

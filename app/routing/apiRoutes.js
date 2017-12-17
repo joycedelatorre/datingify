77034b19b969b9d00a84fe302338f50dd38ffb4a
joyce <joycedelatorre87@gmail.com>
2017-12-17 11:14:37 -0600

    Pass: links to display data(friends.js) & github

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var friends = require("../data/friends");
+var path = require("path"); 
 
 //This way of writing module.exports works too!
 module.exports = function(app){
@@ -7,4 +8,9 @@ module.exports = function(app){
     //push the new Object to a the file friends 
     res.json(newFriend);
   });
+
+  app.get("/api/friends", function(req, res){
+    // res.sendFile(path.join(__dirname, "../data/friends.js"));
+    res.json(friends);
+  })
 }

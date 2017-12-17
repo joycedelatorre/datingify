359577d46321a29dee0f3a65cc9edf4ad8d12f67
joyce <joycedelatorre87@gmail.com>
2017-12-17 11:41:23 -0600

    FIX: friends.js data syntax

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,8 +3,9 @@ var path = require("path");
 
 //This way of writing module.exports works too!
 module.exports = function(app){
-  app.post("/api/new", function(req, res){
+  app.post("/api/friends", function(req, res){
     var newFriend = req.body
+    console.log(newFriend);
     //push the new Object to a the file friends 
     res.json(newFriend);
   });

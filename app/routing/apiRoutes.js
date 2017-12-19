2e516f109e039823fc350c08c0945d329a579220
joyce <joycedelatorre87@gmail.com>
2017-12-19 01:23:42 -0600

    fix bug apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,11 +5,10 @@ var fs = require('file-system');
 //This way of writing module.exports works too!
 module.exports = function(app){
   app.post("/api/friends", function(req, res){
-    var newFriend =req.body;
+    var newFriend = req.body;
     console.log(newFriend);
-    console.log(newFriend.scores);
+    console.log(newFriend["scores[]"]);
     //push the new Object to a the file friends 
-    friends.push(newFriend); 
     // FS module is used to overwrite the old list with a new list and be able to save it.
     // fs.writeFile('app/data/friends.js', JSON.stringify(friends),(err)=> {
     //   if(err) throw err;
@@ -17,13 +16,14 @@ module.exports = function(app){
     // });
       var leastcompatibilityScore = 40;
       var currentCompatibleFriend = 0;
-      var finalScores =[]
+      var finalScores =[];
 
     for(var i =0; i<friends.length; i++){
       var friendScores = friends[i].scores;
+      var currentFriendScores = newFriend["scores[]"];
       var placeholder = 0;
       for(var j=0; j<friendScores.length; j++){
-        var diff = Math.abs(parseInt(friendScores[i])- parseInt(newFriend.scores[i])); 
+        var diff = Math.abs(parseInt(friendScores[i])- parseInt(currentFriendScores[i])); 
         placeholder = diff + placeholder;
       }
       finalScores.push(placeholder);
@@ -35,6 +35,8 @@ module.exports = function(app){
         currentCompatibleFriend = a;
       }
     }
+    newFriend.scores = newFriend["scores[]"];
+    friends.push(newFriend); 
     res.json(friends[currentCompatibleFriend]);
   });
 
